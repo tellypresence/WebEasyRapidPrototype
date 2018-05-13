c4351bdd34745c4dd71415521504bec73a24f3b8
tellypresence <praktique-githubpub01@yahoo.com>
2018-05-13 11:55:52 +0100

    Tag workmanager request

--- webg/src/main/java/com/tellypresence/webg/workmanager/BgImplWorkMgr.kt
+++ webg/src/main/java/com/tellypresence/webg/workmanager/BgImplWorkMgr.kt
@@ -9,10 +9,14 @@ import com.tellypresence.webg.pub.IBgWork
 class BgImplWorkMgr : IBgWork {
     private val TAG = BgImplWorkMgr::class.java.simpleName
 
+    companion object {
+        const val WM_TAG_SCRAPE_WEBSITE = "BgImplWorkMgr::scrape_website"
+    }
 
     override fun scrapeWebsite() {
         Log.d(TAG, "scrapeWebsite()")
         val compressionWork = OneTimeWorkRequest.Builder(BgWorkMgrWorker::class.java)
+                .addTag(WM_TAG_SCRAPE_WEBSITE)
                 .build()
         WorkManager.getInstance().enqueue(compressionWork)
     }
